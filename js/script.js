81715453e241d416b34d668e82e09865dacf3c4a
Marina Yumi Kanadani <99335827+makanadani@users.noreply.github.com>
2024-06-07 17:34:28 -0300

    Altera pagina SOBRE NOS, adiciona fotos e altera CSS

--- js/script.js
+++ js/script.js
@@ -6,3 +6,18 @@ document.addEventListener('DOMContentLoaded', () => {
         navLinks.classList.toggle('open');
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const dropdownToggle = document.querySelector('.dropdown-toggle');
+    const dropdownMenu = document.querySelector('.menu-item.dropdown');
+
+    dropdownToggle.addEventListener('click', function (event) {
+        // Prevent default action
+        event.preventDefault();
+
+        // Check if viewport is mobile
+        if (window.innerWidth <= 768) {
+            dropdownMenu.classList.toggle('open');
+        }
+    });
+});
