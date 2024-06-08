ca9f15925b71dea60b9eaed725e95cf1c80e9397
Marina Yumi Kanadani <99335827+makanadani@users.noreply.github.com>
2024-06-07 23:50:57 -0300

    Adiciona imagens, ajusta css e adiciona páginas do usuario logado

--- js/script.js
+++ js/script.js
@@ -12,12 +12,52 @@ document.addEventListener('DOMContentLoaded', function () {
     const dropdownMenu = document.querySelector('.menu-item.dropdown');
 
     dropdownToggle.addEventListener('click', function (event) {
-        // Prevent default action
         event.preventDefault();
-
-        // Check if viewport is mobile
         if (window.innerWidth <= 768) {
             dropdownMenu.classList.toggle('open');
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const contactForm = document.getElementById('contactForm');
+
+    contactForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const message = document.getElementById('message').value;
+
+        if (name && email && message) {
+            alert('Mensagem enviada com sucesso!');
+            contactForm.reset();
+        } else {
+            alert('Por favor, preencha todos os campos.');
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const registerForm = document.getElementById('registerForm');
+
+    registerForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+        const confirmPassword = document.getElementById('confirmPassword').value;
+
+        if (name && email && password && confirmPassword) {
+            if (password === confirmPassword) {
+                alert('Cadastro realizado com sucesso!');
+                registerForm.reset();
+            } else {
+                alert('As senhas não coincidem.');
+            }
+        } else {
+            alert('Por favor, preencha todos os campos.');
         }
     });
 });
